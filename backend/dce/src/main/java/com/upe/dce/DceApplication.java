baaba3057a2859acfc01918243f2f1d17ccbe6f4
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2022-08-31 18:42:33 -0300

    Initial Commit

---
+++ backend/dce/src/main/java/com/upe/dce/DceApplication.java
@@ -0,0 +1,12 @@
+package com.upe.dce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DceApplication.class, args);
+	}
+}
