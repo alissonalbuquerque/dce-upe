10bdcce14d5100389b5440b04ff3cc30fa647155
Kvojps <josebazjunior09@gmail.com>
2022-09-08 20:00:40 -0300

    adicionando back

---
+++ backend/dce/src/main/java/com/upe/dce/DceApplication.java
@@ -0,0 +1,13 @@
+package com.upe.dce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DceApplication.class, args);
+	}
+
+}
