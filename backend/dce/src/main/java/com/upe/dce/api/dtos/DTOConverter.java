d7438f243dc43e085045d22968ec33403881ea14
Kvojps <josebazjunior09@gmail.com>
2022-09-26 15:33:39 -0300

    adicionando DTOConverter

---
+++ backend/dce/src/main/java/com/upe/dce/api/dtos/DTOConverter.java
@@ -0,0 +1,59 @@
+package com.upe.dce.api.dtos;
+
+import com.upe.dce.core.ocorrencia.Ocorrencia;
+import com.upe.dce.core.ocorrencia.OcorrenciaUsuario;
+import com.upe.dce.core.ocorrencia.TipoOcorrencia;
+import com.upe.dce.core.usuario.Endereco;
+import com.upe.dce.core.usuario.TipoUsuario;
+import com.upe.dce.core.usuario.Usuario;
+
+public class DTOConverter {
+	public OcorrenciaDTO convertToDTO(Ocorrencia ocorrencia) {
+		return null;
+	}
+	
+	public Ocorrencia convertToEntity(OcorrenciaDTO dto) {
+		return null;
+	}
+	
+	public TipoOcorrenciaDTO convertToDTO(TipoOcorrencia tipoOcorrencia) {
+		return null;
+	}
+	
+	public TipoOcorrencia convertToEntity(TipoOcorrenciaDTO dto) {
+		return null;
+	}
+	
+	public OcorrenciaUsuarioDTO convertToDTO(OcorrenciaUsuario ocorrenciaUsuario) {
+		return null;
+	}
+	
+	public OcorrenciaUsuario convertToEntity(OcorrenciaUsuarioDTO dto) {
+		return null;
+	}
+	
+	public UsuarioDTO convertToDTO(Usuario usuario) {
+		return null;
+	}
+	
+	public Usuario convertToEntity(UsuarioDTO dto) {
+		return null;
+	}
+	
+	public TipoUsuarioDTO convertToDTO(TipoUsuario tipoUsuario) {
+		return null;
+	}
+	
+	public TipoUsuario convertToEntity(TipoUsuarioDTO dto) {
+		return null;
+	}
+
+	public EnderecoDTO convertToDTO(Endereco endereco) {
+		return null;
+	}
+	
+	public Endereco convertToEntity(EnderecoDTO dto) {
+		return null;
+	}
+	
+}
