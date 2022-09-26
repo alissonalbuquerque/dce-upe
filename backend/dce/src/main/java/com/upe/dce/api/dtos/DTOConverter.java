cbf73653e3a1320e74ea7d9e822d6060c88b4631
Kvojps <josebazjunior09@gmail.com>
2022-09-26 16:18:56 -0300

    refatorando DTOs com enum && adicionando corpo de metodos de conversao

--- backend/dce/src/main/java/com/upe/dce/api/dtos/DTOConverter.java
+++ backend/dce/src/main/java/com/upe/dce/api/dtos/DTOConverter.java
@@ -3,6 +3,7 @@ package com.upe.dce.api.dtos;
 import com.upe.dce.core.ocorrencia.Ocorrencia;
 import com.upe.dce.core.ocorrencia.OcorrenciaUsuario;
 import com.upe.dce.core.ocorrencia.TipoOcorrencia;
+import com.upe.dce.core.ocorrencia.TipoOcorrenciaEnum;
 import com.upe.dce.core.usuario.Endereco;
 import com.upe.dce.core.usuario.TipoUsuario;
 import com.upe.dce.core.usuario.Usuario;
@@ -11,39 +12,45 @@ public class DTOConverter {
 	public OcorrenciaDTO convertToDTO(Ocorrencia ocorrencia) {
 		return null;
 	}
-	
+
 	public Ocorrencia convertToEntity(OcorrenciaDTO dto) {
 		return null;
 	}
-	
+
 	public TipoOcorrenciaDTO convertToDTO(TipoOcorrencia tipoOcorrencia) {
-		return null;
+		TipoOcorrenciaDTO resultado = TipoOcorrenciaDTO.builder().id(tipoOcorrencia.getId())
+				.tipoOcorrencia(tipoOcorrencia.getTipoOcorrenciaEnum()).build();
+		
+		return resultado;
 	}
-	
+
 	public TipoOcorrencia convertToEntity(TipoOcorrenciaDTO dto) {
-		return null;
+		TipoOcorrencia resultado = TipoOcorrencia.builder().id(dto.getId()).tipoOcorrenciaEnum(dto.getTipoOcorrencia())
+				.build();
+
+		return resultado;
 	}
-	
+
 	public OcorrenciaUsuarioDTO convertToDTO(OcorrenciaUsuario ocorrenciaUsuario) {
 		return null;
 	}
-	
+
 	public OcorrenciaUsuario convertToEntity(OcorrenciaUsuarioDTO dto) {
 		return null;
 	}
-	
+
 	public UsuarioDTO convertToDTO(Usuario usuario) {
 		return null;
 	}
-	
+
 	public Usuario convertToEntity(UsuarioDTO dto) {
 		return null;
 	}
-	
+
 	public TipoUsuarioDTO convertToDTO(TipoUsuario tipoUsuario) {
 		return null;
 	}
-	
+
 	public TipoUsuario convertToEntity(TipoUsuarioDTO dto) {
 		return null;
 	}
@@ -51,9 +58,9 @@ public class DTOConverter {
 	public EnderecoDTO convertToDTO(Endereco endereco) {
 		return null;
 	}
-	
+
 	public Endereco convertToEntity(EnderecoDTO dto) {
 		return null;
 	}
-	
+
 }
